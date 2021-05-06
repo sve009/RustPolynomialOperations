92078ae4fa08cb2a3ca91fa57a2da5d8e8127d50
Sam Eagen <sve9milo@gmail.com>
2021-05-06 18:38:55 -0500

    Overhauled the repl, now statements can be nested. Works pretty well

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,20 @@ use std::collections::HashMap;
 
 enum Item {
     P(Polynomial),
+    Qr((Polynomial, Polynomial)),
     Ps(PolySet),
+    OK,
+}
+
+impl ToString for Item {
+    fn to_string(&self) -> String {
+        match &self {
+            Self::P(p1) => p1.to_string(),
+            Self::Qr((q, r)) => format!("q: {}, r: {}", q.to_string(), r.to_string()),
+            Self::Ps(ps) => ps.to_string(),
+            Self::OK => String::new(),
+        }
+    }
 }
 
 // Currently kinda hacky, need to fully decide on input scheme
@@ -17,6 +30,69 @@ fn get_poly(x: &str, table: &HashMap<String, Item>) -> Polynomial {
         Some(Item::P(p)) => p.clone(),
         None => Polynomial::from_string(x),
         _ => Polynomial { length: 0, terms: vec![] },
+    }
+}
+
+fn prep_ps(x: &str, table: &HashMap<String, Item>) -> Vec<Item> {
+    let s: Vec<&str> = x.split(';').collect();
+    if s.len() == 1 {
+        x.split(' ')
+            .map(|a| a.trim())
+            .map(|a| parse_expression_h(a, table))
+            .collect()
+    } else {
+        s.iter()
+            .map(|a| a.trim())
+            .map(|a| parse_expression_h(a, table))
+            .collect()
+    }
+}
+
+
+
+fn parse_expression_h(x: &str, table: &HashMap<String, Item>) -> Item {
+    if let Some((op, s)) = x.split_once(' ') {
+        if op == "+" {
+            let ps: Vec<Item> = prep_ps(s, table);
+            match (&ps[0], &ps[1]) {
+                (Item::P(p1), Item::P(p2)) => Item::P(add_polys(&p1, &p2)),
+                _ => panic!("The first two arguments were not polynomials"),
+            }
+        } else if op == "-" {
+            let ps: Vec<Item> = prep_ps(s, table);
+            match (&ps[0], &ps[1]) {
+                (Item::P(p1), Item::P(p2)) => Item::P(sub_polys(&p1, &p2)),
+                _ => panic!("The first two arguments were not polynomials"),
+            }
+        } else if op == "*" {
+            let ps: Vec<Item> = prep_ps(s, table);
+            match (&ps[0], &ps[1]) {
+                (Item::P(p1), Item::P(p2)) => Item::P(mult_polys(&p1, &p2)),
+                _ => panic!("The first two arguments were not polynomials"),
+            }
+        } else if op == "/" {
+            let ps: Vec<Item> = prep_ps(s, table);
+            match (&ps[0], &ps[1]) {
+                (Item::P(p1), Item::P(p2)) => Item::Qr(divide_polys(&p1, &p2)),
+                _ => panic!("The first two arguments were not polynomials"),
+            }
+        }
+        else {
+            panic!("Operation not recognized");
+        }
+    } else 
+        Item::P(get_poly(x, table))
+    }
+}
+
+fn parse_expression(x: &str, table: &mut HashMap<String, Item>) -> Item {
+    if let Some(("=", s)) = x.split_once(' ') {
+        if let Some((name, s)) = s.split_once(' ') {
+            table.insert(name.to_string(), parse_expression_h(s, table));
+        }
+        Item::OK
+    } else {
+        parse_expression_h(x, table)
     }
 }
 
@@ -32,40 +108,11 @@ fn main() {
         io::stdin().read_line(&mut s).expect("Failed to read line");
         s = s.trim().to_string();
 
-        println!("You wrote {}", s);
-
-        let commands: Vec<&str> = s.split(' ').collect();
+        println!("{}", parse_expression(&s, &mut items).to_string());
 
         if s == "q" || s == "quit" || s == "exit" {
             break;
         }
 
-        if commands.len() == 3 && commands[0] == "=" {
-            println!("{}", commands[1]);
-            items.insert(commands[1].to_string(), Item::P(Polynomial::from_string(commands[2])));
-        } else if commands.len() == 1 {
-            match items.get(commands[0]) {
-                Some(Item::P(poly)) => println!("{}", poly.to_string()),
-                Some(Item::Ps(set)) => println!("{}", set.to_string()),
-                None => println!("Variable {} not recognized, assign with = first", commands[0]),
-            }
-        } else if commands.len() == 3 && commands[0] == "+" {
-            let p1 = get_poly(commands[1], &items);
-            let p2 = get_poly(commands[2], &items);
-            println!("{}", add_polys(&p1, &p2).to_string());
-        } else if commands.len() == 3 && commands[0] == "-" {
-            let p1 = get_poly(commands[1], &items);
-            let p2 = get_poly(commands[2], &items);
-            println!("{}", sub_polys(&p1, &p2).to_string());
-        } else if commands.len() == 3 && commands[0] == "*" {
-            let p1 = get_poly(commands[1], &items);
-            let p2 = get_poly(commands[2], &items);
-            println!("{}", mult_polys(&p1, &p2).to_string());
-        } else if commands.len() == 3 && commands[0] == "/" {
-            let p1 = get_poly(commands[1], &items);
-            let p2 = get_poly(commands[2], &items);
-            let (q, r) = divide_polys(&p1, &p2);
-            println!("q: {}, r: {}", q.to_string(), r.to_string());
-        }
     }
 }
